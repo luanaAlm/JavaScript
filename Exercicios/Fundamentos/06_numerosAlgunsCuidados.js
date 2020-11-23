833508da5602d062362dcd5ce37cd93ce8f10484
Luana Rodirgues <luanarodrigues3211@gmail.com>
2020-11-23 10:04:22 -0300

    Update 06_numerosAlgunsCuidados.js

---
+++ Exercicios/Fundamentos/06_numerosAlgunsCuidados.js
@@ -0,0 +1,8 @@
+//JS tem o tipoo infinity, de retorno para alguns calculos 
+console.log(7/0)
+//O JS converte o conteudo dentro da string se for numerico pode-se fazer o cálculo 
+console.log("10" / 2)
+// JS não calcula strings que naoo podem ser convertidas em número 
+console.log("show!" * 2)
+//JS gera imprecicões
+console.log(0.1 + 0.7)
