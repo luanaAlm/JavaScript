40dab72b022dbd4b3848c8e6010d7ddc8c52f8b5
Luana Rodirgues <luanarodrigues3211@gmail.com>
2020-12-02 17:31:01 -0300

    Create teste.js

---
+++ Exercicios/PraticaJS/teste.js
@@ -0,0 +1,9 @@
+let PessoaIdentificada = 'Maoh Psycho'
+switch(PessoaIdentificada){
+    case 'Maoh Psycho':
+    case 'General Gailer':
+    console.log("Ativar defesas")
+    break;
+    default:
+        console.log("continuar verificanfo")
+}
