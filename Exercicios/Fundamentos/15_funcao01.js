06208a8966b1b0ae3d494cfe0d89e65584e210cd
Luana Rodirgues <luanarodrigues3211@gmail.com>
2020-12-02 17:30:54 -0300

    Create 15_funcao01.js

---
+++ Exercicios/Fundamentos/15_funcao01.js
@@ -0,0 +1,15 @@
+//Função sem retorno
+function imprimirSoma(a, b){
+    console.log(a+b)
+}
+imprimirSoma(2, 3)
+imprimirSoma(2)
+imprimirSoma(5, 2, 5, 6, 1)
+
+//função com retorno
+function soma(a, b = 0){
+    return a +b
+}
+console.log(soma(2,3))
+console.log(soma(2))
+console.log(soma)
