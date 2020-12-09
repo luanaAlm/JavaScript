5262f9092e135ecc3af024155af57dcad6c9e67a
Luana Rodirgues <luanarodrigues3211@gmail.com>
2020-12-09 15:55:01 -0300

    Create 01_organizacao.js

---
+++ Exercicios/1_Fundamentos/01_organizacao.js
@@ -0,0 +1,9 @@
+//JS é usado em sentença de códigos que podem ou não termonar com (;)
+//É organizado em blocos de código que são delimotados por chaves
+console.log("Essa é uma sentença")
+{
+    {
+        console.log("Olá");
+        console.log("Mundo!")//padrão que será usado
+    }
+}
