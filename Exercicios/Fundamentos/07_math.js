70a8a7f96cbbb7d669aa67cf88c954851bca58b2
Luana Rodirgues <luanarodrigues3211@gmail.com>
2020-12-09 15:57:35 -0300

    Delete 07_math.js

--- Exercicios/Fundamentos/07_math.js
+++
@@ -1,7 +0,0 @@
-//Usando a classe math
-const raio =  5.6
-//Math.pow(raio, 2) é uma função 
-const area = Math.PI * Math.pow(raio, 2)
-console.log(area.toFixed(2))
-//Math é um objeto e sua constante é PI
-console.log(typeof Math)
