0815c9a461dbebd29402dea34676f1393a9c3ee4
Luana Rodirgues <luanarodrigues3211@gmail.com>
2020-12-09 15:56:58 -0300

    Create 36_OperadoresUnarios.js

---
+++ Exercicios/1_Fundamentos/36_OperadoresUnarios.js
@@ -0,0 +1,15 @@
+//operador Unários
+let num1 = 1
+let num2 = 2
+
+//pós fixado
+num1++
+console.log(num1)
+
+//pré fixada
+--num1
+console.log(num1)
+
+console.log(++num1 === num2--)
+console.log(num1 === num2)
+console.log()
