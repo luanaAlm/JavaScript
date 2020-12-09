b27c638229844d872f40e3eb41826c8b41b68333
Luana Rodirgues <luanarodrigues3211@gmail.com>
2020-12-09 15:55:14 -0300

    Create 04_tipagemFraca.js

---
+++ Exercicios/1_Fundamentos/04_tipagemFraca.js
@@ -0,0 +1,9 @@
+//Evite nome genéricos e siglas
+let qualquer = 'legal'
+console.log(qualquer)
+console.log(typeof(qualquer)) //typeof: tipo 
+
+qualquer = 3.15846
+console.log(qualquer)
+console.log(typeof(qualquer))
+
