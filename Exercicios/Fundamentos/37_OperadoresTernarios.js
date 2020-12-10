4457d7388a460e205b4ccaca5b55f5627a579e1d
Luana Rodirgues <luanarodrigues3211@gmail.com>
2020-12-09 22:25:39 -0300

    Delete 37_OperadoresTernarios.js

--- Exercicios/Fundamentos/37_OperadoresTernarios.js
+++
@@ -1,9 +0,0 @@
-//operador Ternários
-/**
- * 1 expressao: nota >= 7 retorna verdadeiro ou falso
- * 2: ? interrogação 
- * 3: 'Reprovado' o que retornara se nao for verdadeiro  
- */
-const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado'
-console.log(resultado(7.7))
-console.log(resultado(6.1))
