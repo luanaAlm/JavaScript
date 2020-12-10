e63b66caf623ab660cd9f445caad9f2d2e85e2c9
Luana Rodirgues <luanarodrigues3211@gmail.com>
2020-12-09 22:25:19 -0300

    Delete 30_destructuring2.js

--- Exercicios/Fundamentos/30_destructuring2.js
+++
@@ -1,9 +0,0 @@
-const [a] = [10]
-console.log(a)
-//desistruturação do array
-const[n1, , n3, ,n5, n6 = 0] = [10, 7, 9, 8]
-console.log(n1, n3, n5, n6)
-
-//desistruturação aninhada
-const[, [ , nota]] = [[, 8, 8], [9, 6, 8]]
-console.log(nota)
