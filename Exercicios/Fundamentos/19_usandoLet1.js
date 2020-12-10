aa1e586fe88969778edb769288bb2e26a9bdb935
Luana Rodirgues <luanarodrigues3211@gmail.com>
2020-12-09 22:24:44 -0300

    Delete 19_usandoLet1.js

--- Exercicios/Fundamentos/19_usandoLet1.js
+++
@@ -1,7 +0,0 @@
-var numero = 1 
-{
-    let numero = 2
-    console.log('dentro=', numero)
-}
-console.log('fora=', numero)
-//o valor dentro do escopo é diferente do valor de numero de fora por conta do (let)
