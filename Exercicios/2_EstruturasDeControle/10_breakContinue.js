a46d921212fff83cbf8d8cbfa95f7faa8081deba
Luana Rodirgues <luanarodrigues3211@gmail.com>
2020-12-09 22:24:34 -0300

    Create 10_breakContinue.js

---
+++ Exercicios/2_EstruturasDeControle/10_breakContinue.js
@@ -0,0 +1,21 @@
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for(let x in nums){ 
+    if (x == 5) {
+        break //atua encima do laço for
+    }
+    console.log(`${x} = ${nums[x]}`)
+}
+console.log('\n')
+for(y in nums){
+    if(y == 5){
+        continue //atua encima do laço for
+    }
+    console.log(`${y} = ${nums[y]}`)
+}
+console.log('\n')
+externo: for (a in nums){
+    for (b in nums){
+        if (a == 2 && b == 3) break externo
+        console.log(`Par = ${a}, ${b}`)
+    }
+}
