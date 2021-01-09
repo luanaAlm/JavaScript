ece08c64733ffaa6cf471a1fee2626bfda3c8f72
Luana Rodirgues <luanarodrigues3211@gmail.com>
2021-01-08 21:45:46 -0300

    Update 11_funcoesCallback.js

--- Exercicios/3_Função/11_funcoesCallback.js
+++ Exercicios/3_Função/11_funcoesCallback.js
@@ -1,8 +1,11 @@
+//Chamar de volta
 const fabricantes = ['Mercedes', 'Audi', 'BMW']
 
 function imprimir(nome, indice){
     console.log(`${indice + 1}. ${nome}`);
 }
 
+//callback: passa uma função e o evento acontece quando for chamado
+//forEach
 fabricantes.forEach(imprimir)
 fabricantes.forEach(a => console.log(a))
