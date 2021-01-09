4fa07480323051b8086b31f5f1adf08ae1ee751e
Luana Rodirgues <luanarodrigues3211@gmail.com>
2021-01-08 23:39:43 -0300

    Create exercicio1.js

---
+++ Exercicios/PraticaJS/exercicio1.js
@@ -0,0 +1,27 @@
+//Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma, subtração, multiplicação e divisão desses valores
+
+function Calculo(num1, num2){
+    this.num1 = num1
+    this.num2 = num2
+    
+    this.subtracao = function(){
+        calc =  num1 - num2
+        return console.log(`Subtração: ${calc}`);
+    }
+
+    this.multiplicacao = function(){
+        calc = num1 * num2
+        return console.log(`Multiplicacao: ${calc}`);
+    }
+
+    this.divisao = function(){
+        calc = num1 * num2
+        return console.log(`Divisao: ${calc}`);
+    }
+}
+
+const c = new Calculo(110, 11)
+c.subtracao()
+c.multiplicacao()
+c.divisao()
+
