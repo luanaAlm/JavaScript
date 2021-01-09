00157fb806300585fe6d954e94ce33a3f36cbfb8
Luana Rodirgues <luanarodrigues3211@gmail.com>
2021-01-09 13:20:22 -0300

    Create 01_OO.js

---
+++ Exercicios/4_IntroducaoOO/01_OO.js
@@ -0,0 +1,15 @@
+//Orientação aa objetos em JS
+
+objeto = {
+    //dentro dos objetos tem:valores, atributos 
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        // ...
+    }
+}
+
+//passase a invocar atraves do dado
+//processamento pertence a objeto ue tem  os valores
+objeto.processamento()
