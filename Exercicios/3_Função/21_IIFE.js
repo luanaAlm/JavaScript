c4bf5fd88120a910ae175e4f0db6e89490fab9d7
Luana Rodirgues <luanarodrigues3211@gmail.com>
2021-01-08 23:21:38 -0300

    Create 21_IIFE.js

---
+++ Exercicios/3_Função/21_IIFE.js
@@ -0,0 +1,7 @@
+//IIFE: Immediately Invoked Function Expression é uma função em JavaScript que é executada assim que definida.
+
+//funcao anonima
+(function(){
+    console.log('Será execultado na hora!');
+    console.log('Foge do escoo mais abrangente');
+})() // A variável nome não é acessível fora do escopo da expressão 
