9e1a58a136c99a498433f81439d686bd596b1396
Luana Rodirgues <luanarodrigues3211@gmail.com>
2021-01-11 15:24:40 -0300

    Create 05_notacaoLiteral.js

---
+++ Exercicios/4_IntroducaoOO/05_notacaoLiteral.js
@@ -0,0 +1,19 @@
+//Notação Literal
+
+const a = 1
+const b = 2
+const c = 3
+
+const nomeAtrib = 'nota'
+const valorAtrib = 7.87
+
+const obj1 = {[nomeAtrib]: valorAtrib}
+console.log(obj1);
+
+const obj2 = {
+    funcao(){
+        // ...
+    }
+}
+
+console.log(obj2);
