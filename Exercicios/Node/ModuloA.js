d9999865a74378efbe35d6742991e286daf36c68
Luana Rodirgues <luanarodrigues3211@gmail.com>
2021-01-13 15:44:56 -0300

    Create ModuloA.js

---
+++ Exercicios/Node/ModuloA.js
@@ -0,0 +1,10 @@
+//Como exportar de um módulo Node para outro arquivo (forma menos comum)
+
+//forma 1
+this.ola = 'Fala Pessoas'
+
+//forma 2
+exports.bemVindo = 'Bem vindo ao Node.js'
+
+//forma 3
+module.exports.ateLogo = 'Até proximo exemplo!'
