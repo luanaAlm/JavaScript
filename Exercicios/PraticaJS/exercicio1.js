9a1901ba267b813dbb6fdd5aa9f761872b960c0a
Luana Rodirgues <luanarodrigues3211@gmail.com>
2021-01-13 00:32:56 -0300

    Update exercicio1.js

--- Exercicios/PraticaJS/exercicio1.js
+++ Exercicios/PraticaJS/exercicio1.js
@@ -1,10 +1,11 @@
 //Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma, subtração, multiplicação e divisão desses valores
 
+
 function Calculo(num1, num2){
     this.num1 = num1
     this.num2 = num2
     
-    this.subtracao = function(){
+    subtracao = function(){
         calc =  num1 - num2
         return console.log(`Subtração: ${calc}`);
     }
