d09c9640f125c70ac91b173cc9c493c65f5d0fc7
Luana Almeida <luanarodrigues3211@gmail.com>
2021-01-14 18:11:14 -0300

    Create ArquivoEscrita.js

---
+++ Exercicios/Node/ArquivoEscrita.js
@@ -0,0 +1,12 @@
+//gerando um arquivo JSON
+const fs = require('fs')
+
+const produto = {
+    nome: 'Celular',
+    preco: 1249.99,
+    desconto: 0.15
+}
+
+fs.writeFile(__dirname + '/ArquivoGerado.json', JSON.stringify(produto), err =>{
+    console.log(err || 'Arquivo salvo!');
+})
